4ab8e57c46970978481b17a09fd8659f5b9b682a
Александр Скляров <aleksandrsklyarov@MacBook-Pro-Aleksandr.local>
2025-10-18 17:01:38 +0300

    STEP-2 add options

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 
-parser.add_argument('first_file', type=str, help='')
-parser.add_argument('second_file', type=str, help='')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', type=str, help='set format of output', default='plain')
 
 args = parser.parse_args()
 
